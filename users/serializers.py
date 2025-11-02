1dadf81a5d891d7dd3c490c291973b2e9a0a60c5
Andrew <demchuk162@gmail.com>
2025-11-02 16:29:47 +0200

    fields in the registration serializer have been adjusted

--- users/serializers.py
+++ users/serializers.py
@@ -8,7 +8,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+        fields = ('username', 'email', 'password',)
 
     def create(self, validated_data):
         user = User(
