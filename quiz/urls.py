90956b00c8fe073af07b3c61a8ec779bc421c765
Andrew <demchuk162@gmail.com>
2025-12-27 16:14:23 +0200

    fix(quiz): add basename to router registry
    Fixes AssertionError in DefaultRouter caused by missing queryset in ViewSet.

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,12 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views import QuizViewSet
-import quiz
 
 app_name = 'quiz'
 
 router = routers.DefaultRouter()
-router.register(r'quizzes', QuizViewSet)
+router.register(r'quizzes', QuizViewSet, basename='quiz')
 
 urlpatterns = [
     path('api/', include(router.urls)),
