b24bcd9b64efaea38fce79f5188adc804051e4aa
Andrew <demchuk162@gmail.com>
2025-12-29 17:45:56 +0200

    feat(user): enhance UserSerializer with favourite_tests and passed_tests_count fields

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-
+from quiz.models import Quiz
 User = get_user_model()
 
 
@@ -25,15 +25,27 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 
+class FavouriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = ("id", 'title', "description")
+
 class UserSerializer(serializers.ModelSerializer):
     avatar = serializers.SerializerMethodField()
 
+    favourite_tests = FavouriteSerializer(many=True)
+
+    passed_tests_count = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ("id", "username", "email", "first_name", "last_name", "avatar")
+        fields = ("id", "username", "email", "first_name", "last_name", "avatar", "about", "favourite_tests", "passed_tests_count")
 
     def get_avatar(self, obj):
         request = self.context.get("request")
         if obj.avatar:
             return request.build_absolute_uri(obj.avatar.url)
         return None
+
+    def get_passed_tests_count(self, obj):
+        return obj.quiz_attempts.filter(completed_at__isnull=False).values("quiz").distinct().count()
