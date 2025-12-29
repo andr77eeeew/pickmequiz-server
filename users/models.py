28c5801d85298dca14b5a98bf616df2790f85e88
Andrew <demchuk162@gmail.com>
2025-12-29 17:45:34 +0200

    feat(user): add 'about' field and 'favourite_tests' relationship to User model

--- users/models.py
+++ users/models.py
@@ -4,3 +4,5 @@ from django.db import models
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to="avatar", null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    favourite_tests = models.ManyToManyField("quiz.Quiz", blank=True, related_name="favoured_by")
