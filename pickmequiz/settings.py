6a4bbffcdf65e6f6ce65ea4e2f8045d4d3c34709
Andrew <demchuk162@gmail.com>
2025-12-31 13:17:14 +0200

    feat(auth): extend access token lifetime to 30 minutes

--- pickmequiz/settings.py
+++ pickmequiz/settings.py
@@ -165,7 +165,7 @@ REST_FRAMEWORK = {
 # SIMPLE JWT
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
