d1018af231503b1bfbf90acad227d96538295d98
Andrew <demchuk162@gmail.com>
2025-12-31 16:30:53 +0200

    feat(user): update user model to enforce unique email and customize avatar upload path

--- users/models.py
+++ users/models.py
@@ -1,8 +1,22 @@
+import os
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+def user_avatar_path(instance, filename):
+    ext = os.path.splitext(filename)[1]
+    if not ext:
+        ext = '.jpg'
+    filename = f"{instance.username}_avatar{ext}"
+    return f'avatars/user_{instance.id}/{filename}'
+
 
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to="avatar", null=True, blank=True)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    avatar = models.ImageField(upload_to=user_avatar_path, null=True, blank=True)
     about = models.TextField(null=True, blank=True)
-    favourite_tests = models.ManyToManyField("quiz.Quiz", blank=True, related_name="favoured_by")
+    favourite_tests = models.ManyToManyField(
+        "quiz.Quiz",
+        blank=True,
+        related_name="favoured_by"
+    )
