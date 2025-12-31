831cb80698416ac7ea60edf04084977b745c3a44
Andrew <demchuk162@gmail.com>
2025-12-31 16:31:22 +0200

    feat(throttling): implement throttling for login attempts and define rate limits

--- pickmequiz/settings.py
+++ pickmequiz/settings.py
@@ -160,6 +160,13 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+
+    'DEFAULT_THROTTLE_CLASSES': [],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '100/hour',
+        'anon': '20/hour',
+        'login': '5/minute',
+    },
 }
 
 # SIMPLE JWT
