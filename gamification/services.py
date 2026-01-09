78e7261c5a8dd5b524db0db267ebea900cb07414
Andrew <demchuk162@gmail.com>
2026-01-09 16:43:19 +0200

    feat(services): add achievement checking service to track new user achievements

---
+++ gamification/services.py
@@ -0,0 +1,26 @@
+from .models import Achievement, UserAchievement
+from .rules import ACHIEVEMENT_RULES
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def check_new_achievements(user, attempt):
+    exist_user_achievements = UserAchievement.objects.filter(user=user)
+    exist_achievement_codes = set(
+        ua.achievement.code for ua in exist_user_achievements
+    )
+    new_achievements = []
+
+    for code, rule_func in ACHIEVEMENT_RULES.items():
+        if code in exist_achievement_codes:
+            continue
+        if rule_func(user, attempt):
+            try:
+                achievement = Achievement.objects.get(code=code)
+                user_achievement = UserAchievement.objects.create(
+                    user=user, achievement=achievement
+                )
+                new_achievements.append(user_achievement)
+            except Achievement.DoesNotExist:
+                logger.error(f"Achievement with code {code} does not exist.")
